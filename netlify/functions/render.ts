ccfafcd122f01720e884b03e7dbf080fcaa8fc46
Tom Jardine-McNamara <tom@itsravenous.com>
2022-10-14 15:18:25 +0100

    debug

--- netlify/functions/render.ts
+++ netlify/functions/render.ts
@@ -1,11 +1,10 @@
 import { Handler } from "@netlify/functions";
 
-console.log(require("child_process").execSync("cat /etc/*-release").toString());
 const handler: Handler = async (event, context) => {
   return {
     statusCode: 200,
     headers: { "content-type": "text/plain" },
-    body: "hello",
+    body: require("child_process").execSync("cat /etc/*-release").toString(),
   };
 };
 
