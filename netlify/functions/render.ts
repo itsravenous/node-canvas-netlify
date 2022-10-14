093361a2688d9d1a694d4f3b1008704538c450e9
Tom Jardine-McNamara <tom@itsravenous.com>
2022-10-14 15:15:16 +0100

    debug

--- netlify/functions/render.ts
+++ netlify/functions/render.ts
@@ -1,28 +1,12 @@
 import { Handler } from "@netlify/functions";
-import { createCanvas } from 'canvas'
-import { fabric } from 'fabric';
 
-console.log(require('child_process').execSync('cat /etc/*-release').toString())
-nst handler: Handler = async (event, context) => {
-  const { width, height, data } = JSON.parse(event.body);
-  const canvas = createCanvas(width, height);
-nst fCanvas = new fabric.StaticCanvas(canvas as any, {
-    width,
-    height
-  });
-nst loadJson = new Promise(resolve => {
-    anvas.loadFromJSON(data, () => {
-      console.log('loaded JSON')
-      resolve(fCanvas.toDataURL())
-    })
-  });
-  const base64 = await loadJson;
-turn {
+console.log(require("child_process").execSync("cat /etc/*-release").toString());
+const handler: Handler = async (event, context) => {
+  return {
     statusCode: 200,
-      headers: { 'content-type': 'text/plain' },
-    body: base64
+    headers: { "content-type": "text/plain" },
+    body: "hello",
   };
 };
 
 export { handler };
-
