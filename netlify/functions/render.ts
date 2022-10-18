88ef8fd0202b12476fec58ee15a7961f56b77737
Tom Jardine-McNamara <tom@itsravenous.com>
2022-10-18 14:23:48 +0100

    debug

--- netlify/functions/render.ts
+++ netlify/functions/render.ts
@@ -2,17 +2,20 @@ import { Handler } from "@netlify/functions";
 import { createCanvas } from "canvas";
 
 const handler: Handler = async (event, context) => {
-  require('child_process').execSync('cp node_modules/canvas/build/Release/libstdc++.so.6 /lib64/')
+  const { execSync } = require("child_process");
+  execSync("cp node_modules/canvas/build/Release/libstdc++.so.6 /lib64/");
+  const ldd = execSync(
+    "ldd node_modules/canvas/build/Release/canvas.node node_modules/canvas/build/Release/*.so*"
+  ).toString();
   const canvas = createCanvas(300, 300);
-  const ctx = canvas.getContext("2d");
-  ctx.fillRect(10, 10, 100, 100);
-  const base64 = canvas.toDataURL();
+  // const ctx = canvas.getContext("2d");
+  // ctx.fillRect(10, 10, 100, 100);
+  // const base64 = canvas.toDataURL();
   return {
     statusCode: 200,
     headers: { "content-type": "text/plain" },
-    body: base64,
+    body: ldd,
   };
 };
 
 export { handler };
-
