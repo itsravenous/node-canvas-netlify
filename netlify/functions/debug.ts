f93e2fe3e6e8c298b126d514d53700ec87231f20
Tom Jardine-McNamara <tom@itsravenous.com>
2022-10-18 11:39:52 +0100

    debug

--- netlify/functions/debug.ts
+++ netlify/functions/debug.ts
@@ -1,17 +1,20 @@
 import { Handler } from "@netlify/functions";
 
 const handler: Handler = async (event, context) => {
-  const linuxVersion = require("child_process")
-    .execSync("cat /etc/*-release")
-    .toString();
-  const nodeVersion = require("child_process")
-    .execSync("node --version")
-    .toString();
+  const { execSync } = require('child_process')
+  const linuxVersion =
+    execSync("cat /etc/*-release")
+      .toString();
+  const nodeVersion =
+    execSync("node --version")
+      .toString();
+  const ldd = execSync("ldd node_modules/canvas/build/Release/canvas.node").toString()
+  const ls = execSync("ls node_modules/canvas/build/Release").toString()
 
   return {
     statusCode: 200,
     headers: { "content-type": "text/plain" },
-    body: linuxVersion + "\n\n" + nodeVersion,
+    body: [linuxVersion, nodeVersion, ldd, ls].join("\n\n")
   };
 };
 
