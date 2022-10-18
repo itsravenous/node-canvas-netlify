944f97dac862262f02cbfbe57ff39d4b2a5c1656
Tom Jardine-McNamara <tom@itsravenous.com>
2022-10-18 14:17:08 +0100

    debug

--- netlify/functions/render.ts
+++ netlify/functions/render.ts
@@ -2,6 +2,7 @@ import { Handler } from "@netlify/functions";
 import { createCanvas } from "canvas";
 
 const handler: Handler = async (event, context) => {
+  require('child_process').execSync('cp node_modules/canvas/build/Release/libstdc++.so.6 /lib64/')
   const canvas = createCanvas(300, 300);
   const ctx = canvas.getContext("2d");
   ctx.fillRect(10, 10, 100, 100);
@@ -14,3 +15,4 @@ const handler: Handler = async (event, context) => {
 };
 
 export { handler };
+
