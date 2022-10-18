577c7360bf10bc7a6ac8027c2fe129a6a2825d61
Tom Jardine-McNamara <tom@itsravenous.com>
2022-10-18 12:17:49 +0100

    debug

--- netlify/functions/debug.ts
+++ netlify/functions/debug.ts
@@ -1,21 +1,21 @@
 import { Handler } from "@netlify/functions";
 
 const handler: Handler = async (event, context) => {
-  const { execSync } = require('child_process')
-  const linuxVersion =
-    execSync("cat /etc/*-release")
-      .toString();
-  const nodeVersion =
-    execSync("node --version")
-      .toString();
-  const ldd = execSync("ldd node_modules/canvas/build/Release/canvas.node").toString()
-  const ls = execSync("ls node_modules/canvas/build/Release").toString()
+const { execSync } = require('child_process')
+const linuxVersion =
+execSync("cat /etc/*-release")
+.toString();
+const nodeVersion =
+execSync("node --version")
+.toString();
+const ldd = execSync("ldd node_modules/canvas/build/Release/canvas.node node_modules/canvas/build/Release/*.so*").toString()
+const ls = execSync("ls node_modules/canvas/build/Release").toString()
 
-  return {
-    statusCode: 200,
-    headers: { "content-type": "text/plain" },
-    body: [linuxVersion, nodeVersion, ldd, ls].join("\n\n")
-  };
+return {
+statusCode: 200,
+headers: { "content-type": "text/plain" },
+body: [linuxVersion, nodeVersion, ldd, ls].join("\n\n")
+};
 };
 
 export { handler };
